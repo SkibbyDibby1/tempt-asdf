dbddcdbe0a72c63b7d77f72696d89e9072dee9f8
SkibbyDibby1 <108428962+SkibbyDibby1@users.noreply.github.com>
2023-10-05 14:16:37 -0500

    Update temp1.py

--- temp1.py
+++ temp1.py
@@ -7,26 +7,17 @@ try:
     for line_number, line in enumerate(lines, start=1):
         line = line.rstrip()
         if any(word in line for word in ["flipped", "toggled", "reversed", "inverted", "switched"]):
-            if line.endswith("safe"):
-                line_parts = line.split(":")
-                if len(line_parts) > 1:
-                    print(line_parts[0].strip(), ":", line_parts[1].strip())
-                else:
-                    print(line)
-            else:
-                if "safe" in line:
-                    line = line.replace("safe", "unsafe")
-                elif "unsafe" in line:
-                    line = line.replace("unsafe", "safe")
-                print(line)
-        else:
-            print(line)
+            if "safe" in line:
+                line = line.replace("safe", "unsafe")
+            elif "unsafe" in line:
+                line = line.replace("unsafe", "safe")
+        print(line)
 
-    safe_line_numbers = [line_parts[0].strip() for line_number, line in enumerate(lines, start=1) if line.endswith("safe") and ":" in line for line_parts in [line.split(":")]]
+    safe_line_numbers = [str(line_number) for line_number, line in enumerate(lines, start=1) if line.startswith("safe")]
     if safe_line_numbers:
-        print("Numbers at the beginning of lines ending with 'safe':", ", ".join(safe_line_numbers))
+        print("Lines starting with 'safe' found at line numbers:", ", ".join(safe_line_numbers))
     else:
-        print("No lines ending with 'safe' found.")
+        print("No lines starting with 'safe' found.")
 
 except FileNotFoundError:
     print(f"File '{file_name}' not found.")
