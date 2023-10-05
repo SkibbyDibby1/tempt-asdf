645c7eead882346c9b5e1b81f29ab8102ba5ce8a
SkibbyDibby1 <108428962+SkibbyDibby1@users.noreply.github.com>
2023-10-05 13:51:16 -0500

    Update temp1.py

--- temp1.py
+++ temp1.py
@@ -2,12 +2,22 @@ file_name = "03_trap_logs.txt"
 
 try:
     with open(file_name, "r") as file:
-        for line in file:
-            line = line.rstrip()
-            if any(word in line for word in ["live", "armed", "ready", "primed", "active"]):
-                line += " unsafe"
-            else:
-                line += " safe"
-            print(line)
+        lines = file.readlines()
+
+    for line_number, line in enumerate(lines, start=1):
+        line = line.rstrip()
+        if any(word in line for word in ["flipped", "toggled", "reversed", "inverted", "switched"]):
+            if "safe" in line:
+                line = line.replace("safe", "unsafe")
+            elif "unsafe" in line:
+                line = line.replace("unsafe", "safe")
+        print(line)
+
+    safe_line_numbers = [str(line_number) for line_number, line in enumerate(lines, start=1) if line.startswith("safe")]
+    if safe_line_numbers:
+        print("Lines starting with 'safe' found at line numbers:", ", ".join(safe_line_numbers))
+    else:
+        print("No lines starting with 'safe' found.")
+
 except FileNotFoundError:
     print(f"File '{file_name}' not found.")
