17d704354f21fbf6536844e7d802413820e2daf8
SkibbyDibby1 <108428962+SkibbyDibby1@users.noreply.github.com>
2023-10-05 07:57:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,30 @@
-n = 1
-s = ""
-j = " "
-keys = open("01_keymaker_ordered.txt", "r")
-while s != j:
+def is_ordered_key(key):
+    # Iterate through each segment in the key
+    for i in range(len(key) - 1):
+        # Compare the current segment with the next segment
+        if key[i] > key[i + 1]:
+            return False
+    return True
 
-    s = keys.readline(n)
-    
-    li = []
-    l = len(s)
-    for i in range (0,l):
-    	li.append(s[i])
-    
-    
-    for i in range(0,l):
-    	for j in range(0,l):
-    		if li[i]<li[j]:
-    			li[i],li[j]=li[j],li[i]
-    j=""
-    
-    for i in range(0,l):
-    	j = j+li[i]
-    	
-    n += 1
-    print(j)
-print("Key found! Key is: " + j)
+def find_ordered_key_from_file(file_path):
+    # Read the keys from the file
+    with open(file_path, 'r') as file:
+        keys = file.read().splitlines()
+
+    # Iterate through each key in the pile
+    for key in keys:
+        # Check if the key is ordered
+        if is_ordered_key(key):
+            return key
+
+    # If no ordered key is found, return None or an appropriate value
+    return None
+
+# Example usage:
+file_path = '01_keymaker_ordered.txt'
+
+ordered_key = find_ordered_key_from_file(file_path)
+if ordered_key:
+    print("Ordered key found:", ordered_key)
+else:
+    print("No ordered key found in the pile.")
